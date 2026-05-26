ffab27929e313d6b6c96da13020efa21ab910b80
C3rv4nt3s <alexandercervantes0413@gmail.com>
2026-05-26 13:09:32 -0500

    feat(taller8): herencia vehiculo y coche

---
+++ src/taller8/Coche.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8;
+
+public class Coche extends Vehiculo {
+
+    private int numeroPuertas;
+
+    public Coche(
+            String marca,
+            double velocidadMaxima,
+            int numeroPuertas
+    ) {
+
+        super(marca, velocidadMaxima);
+
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public void mostrarInformacionCoche() {
+
+        mostrarInformacion();
+
+        System.out.println("Número de puertas: " + numeroPuertas);
+    }
+}
---
+++ src/taller8/MainVehiculo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8;
+
+public class MainVehiculo {
+
+    public static void main(String[] args) {
+
+        Coche coche1 =
+                new Coche(
+                        "Toyota",
+                        280,
+                        4
+                );
+
+        coche1.mostrarInformacionCoche();
+    }
+}
