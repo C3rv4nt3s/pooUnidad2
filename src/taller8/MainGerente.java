0e4c1061cc6cf27f557a5476e62e9d4a8f0d90d4
C3rv4nt3s <alexandercervantes0413@gmail.com>
2026-05-26 14:20:10 -0500

    feat(taller8): herencia empleado y gerente

---
+++ src/taller8/MainGerente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8;
+
+public class MainGerente {
+
+    public static void main(String[] args) {
+
+        Gerente gerente1 =
+                new Gerente(
+                        "Alexander",
+                        4500000,
+                        "Tecnologia"
+                );
+
+        gerente1.mostrarDetalles();
+    }
+}
