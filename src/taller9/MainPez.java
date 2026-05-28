41dee0923224d80e7632d6007a5d53cf94a60f49
C3rv4nt3s <alexandercervantes0413@gmail.com>
2026-05-27 23:41:49 -0500

    feat(taller9): uso de super con animal y pez

---
+++ src/taller9/MainPez.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9;
+
+public class MainPez {
+
+    public static void main(String[] args) {
+
+        Pez pez1 =
+                new Pez(
+                        "Salmón",
+                        "Agua dulce"
+                );
+
+        pez1.mostrarEspecie();
+    }
+}
