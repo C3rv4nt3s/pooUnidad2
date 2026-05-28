732de6519ab532433f20ad067939ab84bda2d1f9
C3rv4nt3s <alexandercervantes0413@gmail.com>
2026-05-27 23:39:42 -0500

    feat(taller9): uso de super con persona y empleado

---
+++ src/taller9/MainEmpleado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9;
+
+public class MainEmpleado {
+
+    public static void main(String[] args) {
+
+        Empleado empleado1 =
+                new Empleado(
+                        "Alexander",
+                        20,
+                        "Tecnología"
+                );
+
+        empleado1.mostrarDetalles();
+    }
+}
