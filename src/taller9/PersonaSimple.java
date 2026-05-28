4da6ad55cdde884f9a2e35747e876b006e80def1
C3rv4nt3s <alexandercervantes0413@gmail.com>
2026-05-27 23:46:03 -0500

    feat(taller9): ejemplo incorrecto del uso de super

---
+++ src/taller9/PersonaSimple.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9;
+
+public class PersonaSimple {
+
+    public void saludar() {
+
+        System.out.println("Hola");
+    }
+}
