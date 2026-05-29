f1d485b663485dc6fd7fe28982c47cd102eb8ce1
C3rv4nt3s <alexandercervantes0413@gmail.com>
2026-05-29 01:58:19 -0500

    feat(taller11): ejemplos incorrectos de clases abstractas

---
+++ src/taller11/EjEmpleadoIncorrecto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11;
+
+public abstract class EjEmpleadoIncorrecto {
+
+    // Mala práctica:
+    // debería ser abstract
+    public double calcularSalario() {
+
+        return 0;
+    }
+}
---
+++ src/taller11/ErrorAbstracto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11;
+
+public class ErrorAbstracto {
+
+    public static void main(String[] args) {
+
+        // ERROR
+        Figura figura1 = new Figura();
+    }
+}
